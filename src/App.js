ed3bf1d59164b3bedef0ed52156b953c10284247
vivekg8338 <gvivekg8338@gmial.com>
2019-04-07 07:24:35 -0700

    check

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  
+successClick(){
+  window.postMessage("https://www.myntra.com/kurtas","*")
+}
+
+doubleClick(){
+  window.postMessage("https://www.myntra.com/ss-19","*")
+}
   render() {
     return (
       <div className="App">
@@ -20,6 +30,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <button onClick={this.successClick.bind(this)}>Success</button>
+        <p>Hiiii</p>
+        <button onClick={this.doubleClick.bind(this)}>Double</button>
       </div>
     );
   }
