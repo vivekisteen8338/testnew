6fcca838693a8f19dba43ad7d1e71909f82b810c
vivekg8338 <gvivekg8338@gmial.com>
2019-04-07 07:17:19 -0700

    Updates

---
+++ precache-manifest.45c05d0b69613712d88ed281da5cd302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testnew/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "efad2e838ef7bc0a6701",
+    "url": "/testnew/static/js/runtime~main.5fd31ee1.js"
+  },
+  {
+    "revision": "959c6a83edbfe82555eb",
+    "url": "/testnew/static/js/main.3c757eb5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/testnew/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "959c6a83edbfe82555eb",
+    "url": "/testnew/static/css/main.8c0019c2.chunk.css"
+  },
+  {
+    "revision": "50b235652d23d6560818ea802bf04496",
+    "url": "/testnew/index.html"
+  }
+];
